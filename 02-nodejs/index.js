7d9189e7a5c11e93554d6a0215c552ddd0f99d60
wasmenezes <was288519@gmail.com>
2020-12-30 06:24:38 -0300

    lesson1 and 2

---
+++ 02-nodejs/index.js
@@ -0,0 +1,67 @@
+/**
+ * 0 Get a user
+ * 1 Get a user phone number by Id
+ * 2 Get user adress by Id
+ */
+
+function getUser(callback) {
+  setTimeout(function () {
+    return callback(null, {
+      id: 1,
+      name: 'Aladin',
+      birth: new Date()
+    })
+  }, 1000)
+}
+
+function getPhone(idUser, callback) {
+  setTimeout(() => {
+    return callback(null, {
+      phone: '999999',
+      ddd: 99
+    })
+  }, 2000)
+}
+
+function getAdress(idUser, callback) {
+  setTimeout(() => {
+    return callback(null, {
+      street: 'street of grace',
+      number: 0
+    })
+  }, 2000);
+}
+
+function resolveUser(erro, user) {
+  console.log('user', user)
+}
+
+getUser(function resolveUser(error, user) {
+  if (error) {
+    console.error('something went wrong: user')
+    return
+  }
+
+  getPhone(user.id, function resolvePhone(error1, phone) {
+    if (error1) {
+      console.error('something went wrong: phone', error1)
+      return
+    }
+
+    getAdress(user.id, function resolveAdress(error2, adress) {
+      if (error2) {
+        console.error('something went wrong: phone', error2)
+        return
+      }
+
+      console.log(`
+        Name: ${user.name},
+        Adress: ${adress.street}, ${adress.number}
+        Phone: (${phone.ddd}) ${phone.phone}
+      `)
+    })
+  })
+})
+//const phone = getPhone(user.id)
+
+//console.log('phone', phone)
