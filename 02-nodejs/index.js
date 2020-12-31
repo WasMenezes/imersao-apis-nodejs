649c70a3e70dc12615abfe88da3c3ac9f5d0fe94
wasmenezes <was288519@gmail.com>
2020-12-31 06:43:55 -0300

    refactor examples with promises

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -4,23 +4,30 @@
  * 2 Get user adress by Id
  */
 
-function getUser(callback) {
-  setTimeout(function () {
-    return callback(null, {
-      id: 1,
-      name: 'Aladin',
-      birth: new Date()
-    })
-  }, 1000)
+ const util = require('util');
+ const getAdressAsync = util.promisify(getAdress)
+
+function getUser() {
+  return new Promise(function resolvePromise(resolve, reject) {
+    setTimeout(function () {
+      return resolve({
+        id: 1,
+        name: 'Aladin',
+        birth: new Date()
+      })
+    }, 1000)
+  })
 }
 
-function getPhone(idUser, callback) {
-  setTimeout(() => {
-    return callback(null, {
-      phone: '999999',
-      ddd: 99
-    })
-  }, 2000)
+function getPhone(idUser) {
+  return new Promise(function resolvePromise(resolve, reject){
+    setTimeout(() => {
+      return resolve({
+        phone: '999999',
+        ddd: 99
+      })
+    }, 2000)
+  })
 }
 
 function getAdress(idUser, callback) {
@@ -32,36 +39,68 @@ function getAdress(idUser, callback) {
   }, 2000);
 }
 
-function resolveUser(erro, user) {
-  console.log('user', user)
-}
+const userPromise = getUser()
 
-getUser(function resolveUser(error, user) {
-  if (error) {
-    console.error('something went wrong: user')
-    return
-  }
-
-  getPhone(user.id, function resolvePhone(error1, phone) {
-    if (error1) {
-      console.error('something went wrong: phone', error1)
-      return
-    }
-
-    getAdress(user.id, function resolveAdress(error2, adress) {
-      if (error2) {
-        console.error('something went wrong: phone', error2)
-        return
+userPromise
+  .then(function (user) {
+    return getPhone(user.id)
+    .then(function resolvePhone(result){
+      return {
+        user: {
+          name: user.name,
+          id: user.id//
+        },
+        phone: result
       }
-
-      console.log(`
-        Name: ${user.name},
-        Adress: ${adress.street}, ${adress.number}
-        Phone: (${phone.ddd}) ${phone.phone}
-      `)
     })
   })
+  .then(function(result) {
+    const adress = getAdressAsync(result.user.id)
+    return adress.then(function resolveAdress(resultAdress) {
+      return {
+        user: result.user,
+        phone: result.phone,
+        adress: resultAdress
+      }
+    });
+  })
+  .then(function (result) {
+    console.log(`
+      Name: ${result.user.name},
+      Adress: ${result.adress.street}, ${result.adress.number}, 
+      Phone: (${result.phone.ddd}) ${result.phone.phone}, 
+    `)
+  })
+  .catch(function (error) {
+    console.error('something went wrong', error)
 })
+
+// getUser(function resolveUser(error, user) {
+//   if (error) {
+//     console.error('something went wrong: user')
+//     return
+//   }
+
+//   getPhone(user.id, function resolvePhone(error1, phone) {
+//     if (error1) {
+//       console.error('something went wrong: phone', error1)
+//       return
+//     }
+
+//     getAdress(user.id, function resolveAdress(error2, adress) {
+//       if (error2) {
+//         console.error('something went wrong: phone', error2)
+//         return
+//       }
+
+//       console.log(`
+//         Name: ${user.name},
+//         Adress: ${adress.street}, ${adress.number}
+//         Phone: (${phone.ddd}) ${phone.phone}
+//       `)
+//     })
+//   })
+// })
 //const phone = getPhone(user.id)
 
 //console.log('phone', phone)
