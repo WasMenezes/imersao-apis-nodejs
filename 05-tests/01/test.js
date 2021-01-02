b922fbb523fb3570f56e601ed63f3effd67fab11
wasmenezes <was288519@gmail.com>
2021-01-02 16:17:13 -0300

    ensure should return r2d2 with correct format using mocha

---
+++ 05-tests/01/test.js
@@ -0,0 +1,11 @@
+const assert = require('assert')
+const { getPeople } = require('./service')
+
+describe('Star Wars Tests', function () {
+  it('ensure return r2d2 with correct format', async() => {
+    const expected = [{ name: 'R2-D2', height: '96'}]
+    const baseName = `r2-d2`
+    const result = await getPeople(baseName)
+    assert.deepStrictEqual(result, expected)
+  })
+})
