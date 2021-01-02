7aa2be5c39babf4cbf7fcdbb50c6216b69b45d4e
wasmenezes <was288519@gmail.com>
2021-01-02 14:33:02 -0300

    array.map hardcoded

--- 04-lists/for-forin/2.map.js
+++ 04-lists/for-forin/2.map.js
@@ -1,5 +1,17 @@
 const service = require('./service')
 
+Array.prototype.myMap = function(callback) {
+  const newArrayMapped = []
+    for (let index = 0; index <= this.length -1; index++) {
+      const result = callback(this[index], index)
+      newArrayMapped.push(result)
+      
+    }
+
+    return newArrayMapped
+}
+
+
 async function main( ) {
   try {
     const results = await service.getPeople('a')
@@ -14,6 +26,10 @@ async function main( ) {
     // })
 
     //const names = results.results.map((people) => people.name)
+
+    const names = results.results.myMap(function(people, index){
+      return people.name
+    })
 
     console.log('names', names)
   } catch (error) {
