d9c48f71d8012c0050649ea10c95001c06e2e9cc
wasmenezes <was288519@gmail.com>
2021-01-03 20:45:01 -0300

    multi datasource with strategy design pattern

---
+++ 07-multi-db/src/db/strategies/interfaces/ICrud.js
@@ -0,0 +1,19 @@
+class ICrud {
+  create(item) {
+    throw new NotImplementadException()
+  }
+
+  read(query) {
+    throw new NotImplementadException()
+  }
+
+  update(id, item) {
+    throw new NotImplementadException()
+  }
+
+  delete(id) {
+    throw new NotImplementadException()
+  }
+}
+
+module.exports = ICrud
