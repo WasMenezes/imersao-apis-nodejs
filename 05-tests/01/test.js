49cfae8401a451f0bff9c54fbe0dc5260b3419c4
wasmenezes <was288519@gmail.com>
2021-01-03 12:28:19 -0300

    fixing chapter5

--- 05-tests/01/test.js
+++ 05-tests/01/test.js
@@ -1,9 +1,47 @@
 const assert = require('assert')
 const { getPeople } = require('./service')
 
+// we intall the nock package to simulate requisitions
+const nock = require('nock')
+
 describe('Star Wars Tests', function () {
-  it('ensure return r2d2 with correct format', async() => {
-    const expected = [{ name: 'R2-D2', height: '96'}]
+  this.beforeAll(() => {
+    const response = {
+      "count": 1,
+      "next": null,
+      "previous": null,
+      "results": [{
+        "name": "R2-D2",
+        "height": "96",
+        "mass": "32",
+        "hair_color": "n/a",
+        "skin_color": "white, blue",
+        "eye_color": "red",
+        "birth_year": "33BBY",
+        "gender": "n/a",
+        "homeworld": "http://swapi.dev/api/planets/8/",
+        "films": ["http://swapi.dev/api/films/1/",
+          "http://swapi.dev/api/films/2/",
+          "http://swapi.dev/api/films/3/",
+          "http://swapi.dev/api/films/4/",
+          "http://swapi.dev/api/films/5/",
+          "http://swapi.dev/api/films/6/"],
+        "species": ["http://swapi.dev/api/species/2/"],
+        "vehicles": [],
+        "starships": [],
+        "created": "2014-12-10T15:11:50.376000Z",
+        "edited": "2014-12-20T21:17:50.311000Z",
+        "url": "http://swapi.dev/api/people/3/"
+      }]
+    }
+
+    nock('https://swapi.dev/api/people')
+      .get('/?search=r2-d2&format=json')
+      .reply(200, response)
+  })
+
+  it('shuold return r2d2 with correct format', async () => {
+    const expected = [{ name: 'R2-D2', height: '96' }]
     const baseName = `r2-d2`
     const result = await getPeople(baseName)
     assert.deepStrictEqual(result, expected)
